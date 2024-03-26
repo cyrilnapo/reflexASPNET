59bcc92867045a4adc8af95dc91e6a69cf90e58a
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-03-26 15:28:21 +0100

    COMPLETE RESTART

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,48 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using MySql.Data.MySqlClient;
 
 namespace Reflex_Project.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
 
-        private readonly string _connectionString;
-
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(ILogger<IndexModel> logger)
         {
-            _connectionString = configuration.GetConnectionString("DefaultConnection"); 
+            _logger = logger;
         }
 
-        public IActionResult OnPost()
-        {
-            string pseudo = Request.Form["pseudo"];
-            string motDePasse = Request.Form["motDePasse"];
-            string confirmerMotDePasse = Request.Form["confirmerMotDePasse"];
-
-            if (motDePasse != confirmerMotDePasse)
-            {
-                return RedirectToPage("/welcome");
-            }
-
-            using (MySqlConnection connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                string query = "INSERT INTO db_reflex.t_users (username, password) VALUES (@username, @password)";
-                using (MySqlCommand command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@username", pseudo);
-                    command.Parameters.AddWithValue("@password", motDePasse);
-                    command.ExecuteNonQuery();
-                }
-            }
-            Response.Cookies.Append("NomUtilisateur", pseudo);
-
-            return RedirectToPage("/welcome"); 
-        }
         public void OnGet()
         {
-            
+
         }
     }
 }
